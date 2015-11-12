bafcfbe947c0c50e9991cde82c8ba4091dd7560c
YuriMarlon <yuriangulosaldana@gmail.com>
2015-11-12 14:04:41 -0500

    MODIFICACION DE ENTITY Y MODELO DE CLASES

--- IMPLEMETANCION/WEB SERVICE Y WEB/WEB/AccesoDatos/Model1.Designer.cs
+++ IMPLEMETANCION/WEB SERVICE Y WEB/WEB/AccesoDatos/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\Canchas\CanchasSoft\AccesoDatos\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\PROYECTO\DeporClub\IMPLEMETANCION\WEB SERVICE Y WEB\WEB\AccesoDatos\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
